e55090681631ad13fda99d8936ceacb89b6dc094
Yash Kapila <yash.kapila18@gmail.com>
2018-09-21 08:19:09 +0100

    Update EloquentJavaScript.js

--- EloquentJavaScript.js
+++ EloquentJavaScript.js
@@ -16,10 +16,11 @@ const sum = arr => arr.reduce((acc, elem) => acc + elem, 0);
 // Reverse an Array without using .reverse() i.e use pure functions
 const reverseArray = arr => arr.reduceRight((acc, elem) => acc.concat(elem), []);
 
-
 // Flatten an array i.e [[1,2,3], [4,5], [6]] -> [1,2,3,4,5,6]
 const flattenArray = arr => arr.reduce((acc, elem) => acc.concat(elem));
 
+// Object.values alternative for finding if an element exists in the object values
+const findValue = (item, obj) => Object.keys(obj).map(el => obj[el]).includes(item);
 
 /*
  * Funtional switch block
@@ -34,11 +35,23 @@ const executeOperation = operation => (typeof operation === 'function' ? operati
 
 const checkOperation = (cases = {}) => (defaultCase = '') => key => executeOperation(Object.prototype.hasOwnProperty.call(cases, key) ? cases[key] : defaultCase);
 
-
 // Evaluate LHS equality with one of the RHS.
 const isNonEmpty = arr => (arr.length !== 0);
 const equality = (lhs, ...rhs) => isNonEmpty(rhs.filter(el => (lhs === el)));
 
-
 // Composition
 const compose = (...fns) => fns.reduceRight((f, g) => (...args) => f(g(...args)));
+
+// Pattern #1
+// Execute an operation only once
+const operation = (function anonymous() {
+  let condition = false;
+   return function callback(args) {
+    if (!condition) {
+      // block of code to execute only once
+      condition = true;
+    }
+  };
+}());
+
+operation(// arguments if needed);
